d45663936062d15579fdb599288db4d9e2c69fdf
Tristan Record <45321591+RecordTristan@users.noreply.github.com>
2020-01-14 10:28:31 +0100

    Add based boss behaviour

---
+++ BulletHell/Assets/Scripts/Entity.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D), typeof(Animator), typeof(SpriteRenderer))]
+public class Entity : MonoBehaviour
+{
+    public Physic physic;
+    public View view;
+    private Animateur anim;
+
+    void Awake()
+    {
+        physic = new Physic();
+        physic.rigidbody = GetComponent<Rigidbody2D>();
+        view = new View();
+        view.display = GetComponent<SpriteRenderer>();
+        anim = new Animateur();
+        anim.animator = GetComponent<Animator>();
+    }
+}
+public class Physic
+{
+    public Rigidbody2D rigidbody;
+}
+public class View
+{
+    public SpriteRenderer display;
+}
+public class Animateur
+{
+    public Animator animator;
+}
