709fa8e20d35f822a1cdd58faaa7b23a6e8475be
PierrickMk <pierrick.maczkowiak@edu.devinci.fr>
2020-01-14 14:30:03 +0100

    Integration Rewired (plugin input)

---
+++ BulletHell/Assets/3C/Scripts/TestInput.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class TestInput : MonoBehaviour
+{
+	public int playerId = 0;
+	public float moveSpeed = 3.0f;
+	private Vector3 moveVector;
+	private Player player;
+	private Rigidbody2D rb;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		rb = GetComponent<Rigidbody2D>();
+		player = ReInput.players.GetPlayer(playerId);
+		player = ReInput.players.GetPlayer(playerId);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		moveVector.x = player.GetAxis("Move Horizontal"); // get input by name or action id
+		moveVector.y = player.GetAxis("Move Vertical");
+
+		rb.velocity = moveVector * moveSpeed;
+	}
+}
